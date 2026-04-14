21e8bb1ff29ddc258e83ee76aa296f154dbf2373
N-Oliveiraa <nivaldofdc2@gmail.com>
2026-04-14 00:47:48 -0300

    adiciona botão GitHub e ajusta layout dos botões no card

---
+++ README.md
@@ -0,0 +1,62 @@
+# 💼 Portfólio Pessoal
+
+Este é um projeto de portfólio pessoal desenvolvido com **HTML, CSS e JavaScript**.
+
+
+---
+
+## 🚀 Tecnologias usadas
+
+![HTML5](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)<br>
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)<br>
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+---
+
+## 🎯 Funcionalidades
+
+- Layout responsivo
+- Menu de navegação
+- Seção Sobre Mim
+- Lista de projetos
+- Formulário de contato
+- Botão de voltar ao topo
+- Tema claro/escuro 
+
+---
+
+## 📁 Estrutura do projeto
+```bash
+portfolio/
+│
+├── assets/
+│ ├── imagens/
+│ ├── styles.css
+│ └── script.js
+│
+├── index.html
+└── README.md
+
+```
+## ▶️ Como executar o projeto
+
+1. Baixe ou clone o repositório:
+
+```bash
+git clone https://github.com/SEU-USUARIO/SEU-REPOSITORIO.git
+```
+
+Acesse a pasta do projeto:
+```bash
+cd SEU-REPOSITORIO
+```
+
+Abra o arquivo index.html no navegador:
+```bash
+Use extensão Live Server no VS Code
+clique com o botão direito em index.html
+cliquem Open with Live Server  ou  Alt+LAlt+O 
+```
+📌 Objetivo
+
+Este projeto foi criado para praticar desenvolvimento web.
---
+++ assets/script.js
@@ -0,0 +1,163 @@
+const menuBtn = document.querySelector("#menu-hamburguer");
+const menu = document.querySelector("#menu");
+
+menuBtn.addEventListener("click", () => {
+    // Só funciona se a tela for menor que 768px
+    if (window.innerWidth <= 768) {
+        menu.classList.toggle("active");
+    }
+});
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
+        menu.classList.remove("active");
+    }
+});
+
+
+const formulario = document.querySelector('#meu-formulario');
+
+function validarFormulario(event) {
+    event.preventDefault(); 
+
+    const nome = document.querySelector('#nome').value;
+    const email = document.querySelector('#email').value;
+    const msg = document.querySelector('#msg').value;
+
+    if (nome === "" || email === "" || msg === "") {
+        alert("Por favor, preencha todos os campos antes de enviar.");
+    } else {
+        alert(`Obrigado, ${nome}! Sua mensagem foi enviada com sucesso.`);
+        formulario.reset(); 
+    }
+}
+
+formulario.addEventListener('submit', validarFormulario);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btnTema = document.querySelector('#btn-tema');
+    const body = document.body;
+
+    const iconMoon = document.querySelector('#icon-moon');
+    const iconSun = document.querySelector('#icon-sun');
+
+    //  Aplicar tema ao carregar
+    function aplicarTemaSalvo() {
+        const temaSalvo = localStorage.getItem('tema');
+
+        if (temaSalvo === 'dark') {
+            body.classList.add('dark-theme');
+            iconMoon.style.display = "none";
+            iconSun.style.display = "inline";
+        } else {
+            body.classList.remove('dark-theme');
+            iconMoon.style.display = "inline";
+            iconSun.style.display = "none";
+        }
+    }
+
+    //  Alternar tema
+    function alternarTema() {
+        body.classList.toggle('dark-theme');
+
+        if (body.classList.contains('dark-theme')) {
+            iconMoon.style.display = "none";
+            iconSun.style.display = "inline";
+            localStorage.setItem('tema', 'dark');
+        } else {
+            iconMoon.style.display = "inline";
+            iconSun.style.display = "none";
+            localStorage.setItem('tema', 'light');
+        }
+    }
+
+    btnTema.addEventListener('click', alternarTema);
+
+    //Executa ao iniciar
+    aplicarTemaSalvo();
+});
+
+const meusProjetos = [
+    {
+        titulo: "Projeto 01 - Viagem pela vila",
+        descricao: "Uma langpage feita com HTML, CSS e JavaScript.",   
+        imagem: "/assets/imagens/projeto1.png",
+        link: "https://n-oliveiraa.github.io/viagens--landing-page/",
+        github: "https://github.com/N-Oliveiraa/viagens--landing-page.git"
+    },
+    {
+        titulo: "Projeto 02 - Contador de cliques",
+        descricao: "Projeto feito com Vite + React.", 
+        imagem: "/assets/imagens/projeto2.png",
+        link: "https://n-oliveiraa.github.io/contador-de-cliques/",
+        github: "https://github.com/N-Oliveiraa/contador-de-cliques.git"
+    },
+    {
+        titulo: "Projeto 03 - Lista de tarefas",
+        descricao: "Projeto feito em React com Context API e localStorage.",   
+        imagem: "/assets/imagens/projeto3.png",
+        link: "https://n-oliveiraa.github.io/aplicacao-de-tarefas/",
+        github: "https://github.com/N-Oliveiraa/aplicacao-de-tarefas.git",
+    }
+];
+
+function renderizarProjetos() {
+    const container = document.querySelector('.projetos-container');
+    
+    container.innerHTML = "";
+
+    meusProjetos.forEach(projeto => {
+        const cardHTML = `
+            <article class="card-projeto">
+                <img src="${projeto.imagem}" alt="${projeto.titulo}">
+
+                <div class="info-projetos">
+                    <h3>${projeto.titulo}</h3>
+                    <p>${projeto.descricao}</p>
+
+                    <div className="botoes-projeto">
+                        <a
+                        href={p.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn-ver-projeto"
+                        >
+                        Ver projeto
+                        </a>
+
+                        <a
+                        href={p.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="btn-github"
+                        >
+                        GitHub
+                        </a>
+                    </div>
+                </div>
+            </article>
+        `;
+        container.innerHTML += cardHTML;
+    });
+}
+
+renderizarProjetos();
+
+const btnTopo = document.querySelector("#btn-topo");
+
+// mostrar botão ao rolar
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 1300) {
+        btnTopo.style.display = "block";
+    } else {
+        btnTopo.style.display = "none";
+    }
+});
+
+// voltar ao topo
+btnTopo.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
